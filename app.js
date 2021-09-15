a87b754c4ef6b1dc5340732b5678a667afa69f57
= <vikashch1998@gmail.com>
2021-09-15 11:37:21 +0530

    que page open

--- app.js
+++ app.js
@@ -26,4 +26,34 @@ browserOpenPrmoise.then(function(browserGivenbyBrowserOpenPrmoise){
 }).then(function(){
     let loginClickPro = page.click("button[data-analytics='LoginPassword']", {delay : 50})
     return loginClickPro ;
+}).then(function(){
+    let clickedAlgoPro = waitAndClick("a[data-attr1='algorithms']" , page);
+    return  clickedAlgoPro ;
+}).then(function (){
+    let cliked_warmCB = waitAndClick("input[value='warmup']", page);
+    return cliked_warmCB;
+}).then(function(){
+    let wait2sPro = page.waitForTimeout(3000);
+    console.log("Clicked");
+    return wait2sPro ;
+}).then(function(){
+    let click_quesPro = waitAndClick(".content--list_body", page);
+    return click_quesPro ;
+}).then(function(){
+    console.log("clicked");
 })
+
+
+function waitAndClick(selector , cpage){
+    return new Promise(function (resolve , reject){
+        let waitforSelecPro = cpage.waitForSelector(selector , {visible : true});
+         waitforSelecPro.then(function(){
+            let clickSelecPro = cpage.click(selector , {visible : true ,delay : 100});
+            return clickSelecPro ;
+         }).then(function(){
+            resolve();
+         }).catch(function (err){
+            reject(err);
+         })
+    })
+}
