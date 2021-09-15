a5139ff93105fddc17e5295eec358d1ad010057b
= <vikashch1998@gmail.com>
2021-09-16 03:22:25 +0530

    reached code editor

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ const puppeteer = require('puppeteer')
 const {answers} = require("./Solns")
 const emailPass = require("./secrets")
 
+const ans = 
+`#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number_of_elements;
+    cin >> number_of_elements;
+    vector <int> array(number_of_elements);
+    int sum_of_array = 0;
+    
+    for(int i = 0; i < number_of_elements; i++){
+       cin >> array[i];
+       sum_of_array += array[i];
+    }
+    
+    cout << sum_of_array;
+    return 0;
+}`;
+
 let loginLink = "https://www.hackerrank.com/auth/login";
 let browserOpenPrmoise = puppeteer.launch({
     headless:false ,
     defaultViewport : null, 
-    args : ["--start-maximized" , "--disable-notifications"]
+    args : ['--start-maximized' , '--disable-notifications']
 });
 let page, browser ;
 browserOpenPrmoise.then(function(browserGivenbyBrowserOpenPrmoise){
     browser = browserGivenbyBrowserOpenPrmoise ;
-    let browserTabOpenPromise = browserGivenbyBrowserOpenPrmoise.newPage();
+    let browserTabOpenPromise = browser.newPage();
     return browserTabOpenPromise;
 }).then(function (tabGievnbyBrowserTabOpenPromise){
     page = tabGievnbyBrowserTabOpenPromise;
@@ -29,18 +48,29 @@ browserOpenPrmoise.then(function(browserGivenbyBrowserOpenPrmoise){
 }).then(function(){
     let clickedAlgoPro = waitAndClick("a[data-attr1='algorithms']" , page);
     return  clickedAlgoPro ;
+}).then(function(){
+    let wait2sPro = page.waitForTimeout(2000);
+    return wait2sPro ;
 }).then(function (){
     let cliked_warmCB = waitAndClick("input[value='warmup']", page);
     return cliked_warmCB;
 }).then(function(){
     let wait2sPro = page.waitForTimeout(3000);
-    console.log("Clicked");
     return wait2sPro ;
 }).then(function(){
-    let click_quesPro = waitAndClick(".content--list_body", page);
-    return click_quesPro ;
+    //let click_quesPro = waitAndClick(".content--list_body", page);
+    let quesArrPro = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled", {delay :100})
+    return quesArrPro ;
+}).then(function(quesArr){
+    //console.log(quesArr);
+    console.log("length : ", quesArr.length);
+    //return quesArr[0].click();
+    let quessolvePro = quesSolver(page , quesArr[0] , ans);
+    return quessolvePro;
 }).then(function(){
-    console.log("clicked");
+    console.log("reached");
+}).catch(function(err){
+    console.log(err)
 })
 
 
@@ -55,5 +85,29 @@ function waitAndClick(selector , cpage){
          }).catch(function (err){
             reject(err);
          })
+    })
+}
+function quesSolver(page , ques , ans){
+    return new Promise(function(resolve , reject){
+        console.log("in quesSolve");
+        let quesclickPro = ques.click();
+        console.log("in quesSolvepro");
+        quesclickPro
+        .then(function(){
+        let getEditorinFocusp = waitAndClick(".monaco-editor.no-user-select.vs", page ) ;
+        return getEditorinFocusp;
+        }).then(function(){
+            return page.keyboard.down("Control" , {delay:100});
+        }).then(function(){
+            return page.keyboard.press("A" , {delay : 50});
+        }).then(function(){
+            return page.keyboard.press("X" , {delay : 50});
+        }).then(function(){
+            return page.keyboard.up("Control" , {delay:100});
+        }).then(function(){
+            resolve();
+        }).catch(function(err){
+            reject(err);
+        })
     })
 }
